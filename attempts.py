bb7a0efe1841c82f07e2e4798910a918c1682f50
sinyee2000 <56106630+sinyee2000@users.noreply.github.com>
2022-02-21 13:11:51 +0800

    Q3, Q4

--- attempts.py
+++ attempts.py
@@ -60,5 +60,58 @@ Suppose the following input is supplied to the program:
 Then, the output should be:
 {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
 
+
+kamus = {}
+for i in range(1,9,1):
+    kamus[i] = i*i
+
+print(kamus.items())
+
 """
 
+"""
+Question 4
+Level 1
+
+Question:
+Write a program which accepts a sequence of comma-separated numbers from console and 
+generate a list and a tuple which contains every number.
+Suppose the following input is supplied to the program:
+34,67,55,33,12,98
+Then, the output should be:
+['34', '67', '55', '33', '12', '98']
+('34', '67', '55', '33', '12', '98')
+
+List is a collection which is ordered and changeable. Allows duplicate members.
+Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+
+# list vs tuple
+
+mylist = []
+
+while True:
+    word = input ("Enter value:")
+    if word == 'q':
+        break
+    mylist.append(word)
+
+print(mylist)
+# Convert to tuple
+print(tuple(mylist))
+
+"""
+
+"""
+Question 5
+Level 1
+
+Question:
+Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.
+Also please include simple test function to test the class methods.
+
+"""
