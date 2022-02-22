baa8c7769103ac7a487e944d45b876a5592868db
sinyee2000 <56106630+sinyee2000@users.noreply.github.com>
2022-02-22 22:54:17 +0800

    q5,6

--- attempts.py
+++ attempts.py
@@ -114,4 +114,86 @@ getString: to get a string from console input
 printString: to print the string in upper case.
 Also please include simple test function to test the class methods.
 
+
+
+class test(object):
+    
+    def __init__(self):
+        self.x = ""
+
+    def getString (self):
+        self.x = input("Enter input:")
+
+    def toUpper(self):
+        x = self.x.upper()
+        print(x)
+
+obj = test()
+obj.getString()
+obj.toUpper()
 """
+"""
+Question 6
+Level 2
+
+Question:
+Write a program that calculates and prints the value according to the given formula:
+Q = Square root of [(2 * C * D)/H]
+Following are the fixed values of C and H:
+C is 50. H is 30.
+D is the variable whose values should be input to your program in a comma-separated sequence.
+
+Example
+Let us assume the following comma separated input sequence is given to the program:
+100,150,180
+The output of the program should be:
+18,22,24
+
+import math 
+c = 50
+h = 30
+d=100
+q = math.sqrt((2*c*d)/h)
+print(int(q))
+
+
+# To use sqrt
+import math 
+
+# Declare list to store inputs
+numlist = []
+
+# Fx to get user inputs
+def getinput():
+
+    while True:
+        num = input("Enter num:")  
+        if (num == 'q'):
+            break
+        numlist.append(num)
+
+# Fx to calculate
+def calc (d):
+    c = 50
+    h = 30
+    return int(math.sqrt((2*c*d)/h))
+
+def main():
+    getinput()    
+    for x in range(len(numlist)):
+        temp = int(numlist[x])
+        q = calc (temp)
+        print("Output "+ str(x) +": " + str(q))
+
+if __name__ == "__main__":
+    main()
+"""
+import math
+c=50
+h=30
+value = []
+items=[x for x in input().split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+
+print(','.join(value))
