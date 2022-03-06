d199c39d72f2ddc8e55cc5e1df6ea6e3885b380b
sinyee2000 <56106630+sinyee2000@users.noreply.github.com>
2022-03-06 12:42:41 +0800

    Add q7

--- attempts.py
+++ attempts.py
@@ -188,12 +188,16 @@ def main():
 if __name__ == "__main__":
     main()
 """
-import math
-c=50
-h=30
-value = []
-items=[x for x in input().split(',')]
-for d in items:
-    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+"""
+Question 7
+Level 2
 
-print(','.join(value))
+Question:
+Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array. The element value in the i-th row and j-th column of the array should be i*j.
+Note: i=0,1.., X-1; j=0,1,¡­Y-1.
+Example
+Suppose the following inputs are given to the program:
+3,5
+Then, the output of the program should be:
+[[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]] 
+"""
